936537ffbf9c07224cf5bfd8ea4eb0435d1783e7
Jason Bornhoft <jmvbxx@gmail.com>
2020-07-26 21:21:32 -0500

    Add second example employee and format output

--- severance.rb
+++ severance.rb
@@ -1,8 +1,17 @@
 require_relative 'calculations'
 
 employee1 = Calculations.new
-puts employee1.bonuses
-puts employee1.savings
-puts employee1.interest_on_savings
-puts employee1.vacation
-puts employee1.total
+puts 'Total severance due:'
+puts "\tBonuses: #{employee1.bonuses}"
+puts "\tSavings: #{employee1.savings}"
+puts "\tInterest on savings: #{employee1.interest_on_savings}"
+puts "\tVacation: #{employee1.vacation}"
+puts "\tTotal: #{employee1.total}"
+
+employee2 = Calculations.new(958730, 80)
+puts 'Total severance due:'
+puts "\tBonuses: #{employee2.bonuses}"
+puts "\tSavings: #{employee2.savings}"
+puts "\tInterest on savings: #{employee2.interest_on_savings}"
+puts "\tVacation: #{employee2.vacation}"
+puts "\tTotal: #{employee2.total}"
