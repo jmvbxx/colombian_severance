f67c9a08a9a7934806ebc37da1ebfbacfdbdac8d
Jason Bornhoft <jmvbxx@gmail.com>
2020-07-26 19:35:10 -0500

    Add basic calculations

--- calculations.rb
+++ calculations.rb
@@ -1,5 +1,27 @@
 # collections of payroll calculations according to Colombian law
-
 class Calculations
+  def initialize(salary = 1_000_000, days_worked = 180)
+    @salary = salary
+    @days_worked = days_worked
+  end
 
+  def bonuses
+    (@salary * @days_worked) / 360
+  end
+
+  def savings
+    bonuses
+  end
+
+  def interest_on_savings
+    (savings * @days_worked * 0.12) / 360
+  end
+
+  def vacation
+    (@salary * @days_worked) / 720
+  end
+
+  def total
+    bonuses + savings + interest_on_savings + vacation
+  end
 end
--- liquidacion.rb
+++ liquidacion.rb
@@ -1,2 +1,8 @@
 require_relative 'calculations'
 
+employee1 = Calculations.new
+puts employee1.bonuses
+puts employee1.savings
+puts employee1.interest_on_savings
+puts employee1.vacation
+puts employee1.total
