aadd381ed76153c69a93ab0f4320299e4ad064b5
Jason Bornhoft <jmvbxx@gmail.com>
2020-07-30 17:34:46 -0500

    Add comment to show default values

--- severance.rb
+++ severance.rb
@@ -1,6 +1,6 @@
 require_relative 'calculations'
 
-employee1 = Calculations.new
+employee1 = Calculations.new # default values (minimum salary and 180 days worked)
 employee1.to_s
 
 employee2 = Calculations.new(salary: 958_730, start_date: '2020-01-01', end_date: '2020-03-21')
