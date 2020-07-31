8f0d3c6ba93cfb9f22e2d69acde188aaaf272f62
Jason Bornhoft <jmvbxx@gmail.com>
2020-07-31 09:25:47 -0500

    Clean up examples

--- severance.rb
+++ severance.rb
@@ -4,8 +4,5 @@ require 'logger'
 logger = Logger.new(STDOUT)
 logger.level = Logger::INFO
 
-employee1 = Calculations.new # default values (minimum salary and 180 days worked)
-logger.info(employee1.to_s)
-
-employee2 = Calculations.new(salary: 958_730, start_date: '2020-01-01', end_date: '2020-03-21')
-logger.info(employee2.to_s)
+employee = Calculations.new(salary: 958_730, start_date: '2020-01-01', end_date: '2020-03-21')
+logger.info(employee.to_s)
